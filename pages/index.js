e1aae7f7cf1451e4564f933eec760398a8dfd176
JoZaGithub <120468452+JoZaGithub@users.noreply.github.com>
2024-03-15 19:55:35 -0400

    Update index.js
    Thanks ChatGPT!

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my" + hi + "app!" />
+        <Header title={`Welcome to my ${hi} app!`} />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
